b2658a48d73178785db4f197529a1eabf314af53
Dev Kalra <kalradev@outlook.com>
2020-05-07 19:07:58 +0530

    zsh

---
+++ scripts/zsh.zsh
@@ -0,0 +1,24 @@
+# oh-my-zsh
+# dependency - zsh, curl, wget, git
+echo "Installing Ohmyzsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# antigen - plugin manager for zsh
+echo "Installing Antigen"
+curl -L git.io/antigen > antigen.zsh
+mv antigen.zsh $HOME/.antigen.zsh
+
+echo "Installing Z"
+git clone https://github.com/rupa/z.git
+mv ./z/z.sh $HOME/.z.sh
+rm -rf z
+
+echo "Adding config files to home directory"
+# 1 zshrc file
+if [ -f $HOME/.zshrc ] ; then
+    rm $HOME/.zshrc
+fi
+ln -S -T "$(pwd)/../config/zshrc" $HOME/.zshrc
+
+# 2 antigenrc file
+ln -S -T "$(pwd)/../config/antigenrc" $HOME/.antigenrc
