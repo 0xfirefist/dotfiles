8afb0ad376d8a166d07c57236fb5fc0075adb3f6
Dev Kalra <kalradev@outlook.com>
2020-05-17 20:46:38 +0530

    quotes - when starting terminal

--- zsh/debian.sh
+++ zsh/debian.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 # zsh, curl, wget, git
-
 sudo apt-get update && sudo apt install -y zsh curl wget git colorls
+
+# installing motivate
+git clone https://github.com/mubaris/motivate.git
+sudo ./motivate/motivate/install.sh
