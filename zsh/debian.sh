d9528f59bf90ed779e7e313b87a7d654a13aa653
Dev Kalra <kalradev@outlook.com>
2020-05-17 20:47:44 +0530

    motivate - delete cloned repo

--- zsh/debian.sh
+++ zsh/debian.sh
@@ -6,3 +6,4 @@ sudo apt-get update && sudo apt install -y zsh curl wget git colorls
 # installing motivate
 git clone https://github.com/mubaris/motivate.git
 sudo ./motivate/motivate/install.sh
+rm -rf ./motivate
