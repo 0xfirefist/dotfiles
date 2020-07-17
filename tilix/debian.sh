306f5d783563b00651d6cbd1f51f626cbf25d3fe
Dev Kalra <kalradev@outlook.com>
2020-07-17 15:19:04 +0530

    create directory

--- tilix/debian.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# install tilix
-sudo apt install tilix
-
-# install hack nerd font
-echo "do install nerd fonts"
--- tilix/tilix.sh
+++ tilix/tilix.sh
@@ -4,6 +4,9 @@ if [ -d $HOME/.config/tilix/schemes ]; then
    rm -rf $HOME/.config/tilix/schemes
 fi
 
+# create directory
+mkdir -p $HOME/.config/tilix
+
 ln -s -T "$(pwd)/schemes" "$HOME/.config/tilix/schemes"
 
 dconf load /com/gexperts/Tilix/ < tilix.conf
