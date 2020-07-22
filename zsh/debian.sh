e826670023dd77a949f225c0a70c2c7f782ae0af
Dev Kalra <kalradev@outlook.com>
2020-07-22 21:08:41 +0530

    debian file removed

--- zsh/debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# zsh, curl, wget, git
-sudo apt-get update && sudo apt install -y zsh curl wget git colorls
-
-# installing motivate
-git clone https://github.com/mubaris/motivate.git
-sudo ./motivate/motivate/install.sh
-rm -rf ./motivate
