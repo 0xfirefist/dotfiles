78173661f4877a375a4d7e6c8dfa5889f34afe8c
Dev Kalra <kalradev@outlook.com>
2020-07-25 23:50:39 +0530

    plugin manager changed to zinit

--- zsh/zsh.zsh
+++ zsh/zsh.zsh
@@ -1,15 +1,5 @@
 #!/bin/zsh
 
-# oh-my-zsh
-# dependency - zsh, curl, wget, git
-echo "Installing Ohmyzsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-# antigen - plugin manager for zsh
-echo "Installing Antigen"
-curl -L git.io/antigen > antigen.zsh
-mv antigen.zsh $HOME/.antigen.zsh
-
 echo "Installing Z"
 git clone https://github.com/rupa/z.git
 mv ./z/z.sh $HOME/.z.sh
@@ -22,9 +12,6 @@ if [ -f $HOME/.zshrc ] ; then
 fi
 ln -s -T "$(pwd)/zshrc" $HOME/.zshrc
 
-# 2 antigenrc file
-ln -s -T "$(pwd)/antigenrc" $HOME/.antigenrc
-
 # setting zsh as default shell
 echo "Set zsh as your default shell"
 chsh -s /bin/zsh $(whoami)
@@ -33,7 +20,7 @@ chsh -s /bin/zsh $(whoami)
 rm -rf "$HOME/.config/colorls"
 ln -s -T "$(pwd)/colorls" "$HOME/.config/colorls"   
 
-
+source $HOME/.zshrc
 
 
 
