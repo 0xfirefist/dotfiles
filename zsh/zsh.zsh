bd0ecb9a56aecfd522bfc15b97bd3b85de8f5fb7
Dev Kalra <kalradev@outlook.com>
2020-08-31 17:58:31 +0530

    removed colorls and z

--- zsh/zsh.zsh
+++ zsh/zsh.zsh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-echo "Installing Z"
-git clone https://github.com/rupa/z.git
-mv ./z/z.sh $HOME/.z.sh
-rm -rf z
-
 echo "Adding config files to home directory"
 # 1 zshrc file
 if [ -f $HOME/.zshrc ] ; then
@@ -16,18 +11,5 @@ ln -s -T "$(pwd)/zshrc" $HOME/.zshrc
 echo "Set zsh as your default shell"
 chsh -s /bin/zsh $(whoami)
 
-# adding colorls config
-rm -rf "$HOME/.config/colorls"
-ln -s -T "$(pwd)/colorls" "$HOME/.config/colorls"   
-
 source $HOME/.zshrc
-
-
-
-
-
-
-
-
-
 
