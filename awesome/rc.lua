829ad60dd4547c8c8be7472e53c174f320c3272b
Dev Kalra <kalradev@outlook.com>
2020-09-05 09:48:47 +0530

    added screenshots

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -276,6 +276,8 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
+    awful.key({ }, "Print", function () awful.util.spawn("escrotum /home/dev/screenshots/%Y-%m-%d-%H%M%S_$wx$h_escrotum.png") end), 
+    awful.key({ "Control" }, "Print", function () awful.util.spawn("escrotum -s /home/dev/screenshots/%Y-%m-%d-%H%M%S_$wx$h_escrotum.png") end), 
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
